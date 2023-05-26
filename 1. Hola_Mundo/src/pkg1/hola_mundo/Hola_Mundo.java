979fc2cafba6c71f763a640bc8c950a74e84d219
MARCO <chispitas944@gmail.com>
2023-05-25 23:35:21 -0600

    Se Agrega Hola Mundo

---
+++ 1. Hola_Mundo/src/pkg1/hola_mundo/Hola_Mundo.java
@@ -0,0 +1,9 @@
+package pkg1.hola_mundo;
+
+public class Hola_Mundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Marco Antonio De Jesus Mejia");
+    }
+}
